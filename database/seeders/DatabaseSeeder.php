cb7b0e362a9f1f878621419a4030093561604e1f
KamalBdran <khajbdran@gmail.com>
2024-08-05 11:33:43 +0300

    95

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        $this->call(RoleSeeder::class);
+        $this->call(AnimalTypeSeeder::class);
+        $this->call(DepartmentSeeder::class);
     }
 }
